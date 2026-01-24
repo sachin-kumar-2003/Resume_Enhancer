260798f010ea7a088fa58fd72d9ecfb92e7d616b
sachin kumar <goputen215@gmail.com>
2026-01-24 09:45:19 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   
   const [enhancedMarkdown, setEnhancedMarkdown] = useState('');
 
-  const server_url = import.meta.env.VITE_SERVER_URL || "http://127.0.0.1:8000/upload";
+  const server_url = import.meta.env.VITE_SERVER_URL;
 
   const handleSubmit = async () => {
     if (!resumeFile || !jobDescription) {
